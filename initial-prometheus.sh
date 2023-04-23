f9fa628a4fdb3b2b312e35930abb114a15ed1f8a
pjames <pjames@gmail.com>
2023-04-23 21:24:04 +0700

    Create prometheus.yaml on the fly

--- initial-prometheus.sh
+++ initial-prometheus.sh
@@ -5,6 +5,8 @@ set -o allexport; source ./.env; set +o allexport
 SRC_FILE=00-configs/prometheus-tpl.yaml
 DST_FILE=00-configs/prometheus.yaml
 
+cp ${SRC_FILE} ${DST_FILE}
+
 sed -i "s#<<REMOTE_PROM_URL>>#${REMOTE_PROM_URL}#g" ${DST_FILE}
 sed -i "s#<<REMOTE_PROM_USER>>#${REMOTE_PROM_USER}#g" ${DST_FILE}
 sed -i "s#<<REMOTE_PROM_PASSWORD>>#${REMOTE_PROM_PASSWORD}#g" ${DST_FILE}
